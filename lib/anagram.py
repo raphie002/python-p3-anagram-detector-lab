379163a61c545896d139602ad2f59957e73812f5
Raphael Njoroge <raphael.njoroge@student.moringaschool.com>
2025-11-13 15:08:21 +0300

    First commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,24 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.original_word = word
+        self.canonical = self._get_canonical_form(word)
+
+    def _get_canonical_form(self, word):
+        return "".join(sorted(word.lower()))
+
+    def match(self, candidates):
+        matches = []
+        
+        original_lower = self.original_word.lower()
+        
+        for candidate in candidates:
+            candidate_lower = candidate.lower()
+            if original_lower != candidate_lower:
+                candidate_canonical = self._get_canonical_form(candidate)
+                
+                if self.canonical == candidate_canonical:
+                    matches.append(candidate)
+                    
+        return matches
--- lib/debug.py
+++ lib/debug.py
@@ -3,4 +3,4 @@
 from anagram import Anagram
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    import ipdb; ipdb.set_trace() # type: ignore
